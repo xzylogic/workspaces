7f62e5c17b06a9669463c39af671a6c447476e4f
xzylogic <18720930167@163.com>
2017-11-20 11:51:29 +0800

    workspaces起始

---
+++ React/gallery-by-react/src/components/Main.js
@@ -0,0 +1,23 @@
+require('normalize.css/normalize.css');
+require('styles/App.css');
+
+import React from 'react';
+
+let yeomanImage = require('../images/yeoman.png');
+
+class AppComponent extends React.Component {
+  render() {
+    return (
+      <div className="index">
+        <img src={yeomanImage} alt="Yeoman Generator" />
+        <span>Hello</span>
+        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
+      </div>
+    );
+  }
+}
+
+AppComponent.defaultProps = {
+};
+
+export default AppComponent;
