6108e5adb681863c7b3926b77aea2889e6c55045
xzylogic <18720930167@163.com>
2017-11-24 15:41:22 +0800

    第三章3-3至3-5节(改了个音乐)

--- React/react-music-player/app/root.js
+++ React/react-music-player/app/root.js
@@ -13,8 +13,8 @@ let Root=React.createClass({
         $("#player").jPlayer({
             ready:function(){
                 $(this).jPlayer('setMedia',{
-                    // mp3:'http://tingwa.oss-cn-shanghai.aliyuncs.com/2017-01/05/20170105123005-MzkzMTcz.mp3?OSSAccessKeyId=3b1nzo7roav1h50rcp0a35nw&Expires=1511506386&Signature=WmcqXQ2v4wIMjuK1pOlvCwScLZ4%3D'
-                    mp3:'http://oj4t8z2d5.bkt.clouddn.com/%E9%AD%94%E9%AC%BC%E4%B8%AD%E7%9A%84%E5%A4%A9%E4%BD%BF.mp3'
+                    mp3:'http://tingwa.oss-cn-shanghai.aliyuncs.com/2017-01/05/20170105123005-MzkzMTcz.mp3?OSSAccessKeyId=3b1nzo7roav1h50rcp0a35nw&Expires=1511506386&Signature=WmcqXQ2v4wIMjuK1pOlvCwScLZ4%3D'
+                    // mp3:'http://oj4t8z2d5.bkt.clouddn.com/%E9%AD%94%E9%AC%BC%E4%B8%AD%E7%9A%84%E5%A4%A9%E4%BD%BF.mp3'
                 }).jPlayer('play');
             },
             supplied:'mp3',
